e6d5e94a7c44aa0aaa5b324ee966ed29325d4714
小島 雅晴 <fpc-kojima@fp-consulting.co.jp>
2017-08-01 11:32:51 +0900

    19

--- config/database.php
+++ config/database.php
@@ -41,7 +41,8 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            /*'host' => env('DB_HOST', '127.0.0.1'),*/
+host: <%= ENV['DB_HOST'] || ENV['MYSQL_ROOT_HOST'] || '127.0.0.1' %>
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
